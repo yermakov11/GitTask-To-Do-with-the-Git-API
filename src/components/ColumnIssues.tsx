64d84193e681841d9f751df890cf5a2ce7a5de99
unknown <thumpel81@gmail.com>
2024-05-20 20:15:28 +0100

    fix delete button

--- src/components/ColumnIssues.tsx
+++ src/components/ColumnIssues.tsx
@@ -1,44 +1,55 @@
-import { useState,useEffect } from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import { GitTask } from "../api/apiGitTask";
-import Issue from "./Issue";
+import { useState, useEffect } from "react";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
 
-
-
-interface ColumnIssuesProps {
-  dataIssue: GitTask[];
+interface PropsIssue {
+  id: number;
+  title: string;
+  body: string;
 }
 
-export default function ColumnIssues({ dataIssue }: ColumnIssuesProps) {
-  const [deleteDataIssue, setDeleteDataIssue] = useState<GitTask[]>(dataIssue);
+interface ColumnIssuesProps {
+  issues: PropsIssue[];
+}
 
-  const deleteIssue = () => {
-    setDeleteDataIssue([]);
-  };
-
-  useEffect(() => {
-    console.log(deleteDataIssue);
-  }, [deleteDataIssue]);
+export default function ColumnIssues({ issues }: ColumnIssuesProps) {
+  const [deleteIssue, setDeleteIssue] = useState<PropsIssue[]>(issues);
   
+  useEffect(() => {
+    setDeleteIssue(issues);
+  }, [issues]);
+
   return (
     <Container>
       <Row className="justify-content-around align-items-center">
-        <Col sm={4}
+        <Col
+          sm={4}
           className="d-flex flex-column align-items-center bg-danger bg-opacity-25 p-3 rounded overflow-auto"
-          style={{ textAlign:'center',height: "400px",width:'300px'}}
+          style={{ textAlign: "center", height: "400px", width: "300px" }}
         >
           <h3>Issues</h3>
-          <Issue issues={dataIssue} deleteIssue={deleteIssue}/>
+          {deleteIssue.length > 0 && (
+            <Card style={{ backgroundColor: "transparent" }}>
+              {deleteIssue.map((item) => (
+                <Card.Body key={item.id} style={{ border: "black 1px solid" }}>
+                  <Card.Title>{item.title}</Card.Title>
+                  <Card.Text>{item.body}</Card.Text>
+                </Card.Body>
+              ))}
+              <Button variant="danger" onClick={()=>setDeleteIssue([])}>Delete all issues</Button>
+            </Card>
+          )}
         </Col>
-        <Col sm={4}
+        <Col
+          sm={4}
           className="d-flex flex-column align-items-center bg-success bg-opacity-25 p-3 rounded"
-          style={{ textAlign:'center',height: "400px",width:'300px'}}
+          style={{ textAlign: "center", height: "400px", width: "300px" }}
         >
           <h3>In process</h3>
         </Col>
-        <Col sm={4}
+        <Col
+          sm={4}
           className="d-flex flex-column align-items-center bg-primary bg-opacity-25 p-3 rounded"
-          style={{ textAlign:'center',height: "400px",width:'300px'}}
+          style={{ textAlign: "center", height: "400px", width: "300px" }}
         >
           <h3>Done</h3>
         </Col>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
   return (
     <div>
       <InputApi setDataIssue={setDataApi}/>
-      <ColumnIssues dataIssue={dataApi}/>
+      <ColumnIssues issues={dataApi}/>
     </div>
   );
 }
